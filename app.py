14677b68f2fa3b4fc77b1fd166f2e3584b237d4d
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-11-30 16:14:19 +0530

    update

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ import plotly.express as px
 
 st.set_page_config(page_title="AI Podcast Topic Classifier", layout="wide")
 st.title("🎧 AI-Powered Podcast Topic Classifier & Recommender")
+
+st.write("Type a podcast episode summary and let AI detect the topic + recommend similar episodes!")
